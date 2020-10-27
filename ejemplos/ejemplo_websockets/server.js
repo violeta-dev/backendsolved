34aa6c6ff16d57f9085b77937490548764ac6a0c
jamg <jamg44@gmail.com>
2020-10-27 21:44:13 +0100

    websockets: implementamos las conexiones con socket.io

--- ejemplos/ejemplo_websockets/server.js
+++ ejemplos/ejemplo_websockets/server.js
@@ -13,3 +13,26 @@ app.get('/', (req, res, next) => {
 server.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
+const io = require('socket.io')(server);
+
+// ante cada conexión de un cliente...
+io.on('connection', socket => {
+  // socket es una conexión con un browser
+  console.log('nueva conexión de un cliente!', socket.id);
+
+  // recibimos eventos 'nuevo-mensaje'
+  socket.on('nuevo-mensaje', data => {
+    console.log('recibido:', data);
+
+    // enviamos el mensaje recibido de un cliente
+    // al resto de clientes
+    io.emit('mensaje-chat', data);
+
+  });
+
+  setInterval(() => {
+    socket.emit('pasa-un-segundo');
+  }, 1000);
+
+})
